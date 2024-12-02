59c17e6c98d7a7e736e87a56499d335a3d51516b
Danylo <lomporia@gmail.com>
2024-12-02 20:57:58 +0200

    solved fr

--- app/main.py
+++ app/main.py
@@ -1,31 +1,38 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> float:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         price = (
-            car.comfort_class *
-            (self.clean_power - car.clean_mark) *
-            self.average_rating /
-            self.distance_from_city_center
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
         )
         return round(price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None :
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars):
+    def serve_cars(self, cars: int) -> None:
         total_income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -33,10 +40,8 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def rate_service(self, rating):
+    def rate_service(self, rating: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         total_rating += rating
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
-
-
